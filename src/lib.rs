c59f3e5cc63e634015616372a302cc4672c4d922
TeamMeng <862915265@qq.com>
2025-12-25 12:17:40 +0800

    feature: build api skeleton

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+mod config;
+mod handlers;
+
+use crate::handlers::{
+    create_chat_handler, delete_chat_handler, index_handler, list_chat_handler,
+    list_messages_handler, send_message_handler, signin_handler, signup_handler,
+    update_chat_handler,
+};
+use axum::{
+    Router,
+    routing::{get, patch, post},
+};
+pub use config::AppConfig;
+use std::{ops::Deref, sync::Arc};
+
+#[derive(Debug, Clone)]
+pub(crate) struct AppState {
+    inner: Arc<AppStateInner>,
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub(crate) struct AppStateInner {
+    pub(crate) config: AppConfig,
+}
+
+pub fn get_router(config: AppConfig) -> Router {
+    let state = AppState::new(config);
+
+    let api = Router::new()
+        .route("/signin", post(signin_handler))
+        .route("/signup", post(signup_handler))
+        .route(
+            "/chat",
+            get(list_chat_handler)
+                .post(create_chat_handler)
+                .patch(update_chat_handler)
+                .delete(delete_chat_handler),
+        )
+        .route(
+            "/chat/{id}",
+            patch(update_chat_handler)
+                .get(list_chat_handler)
+                .post(send_message_handler),
+        )
+        .route("/chat/{id}/messages", get(list_messages_handler));
+
+    Router::new()
+        .route("/", get(index_handler))
+        .nest("/api", api)
+        .with_state(state)
+}
+
+// when use state.config == state.inner.config
+impl Deref for AppState {
+    type Target = AppStateInner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl AppState {
+    pub fn new(config: AppConfig) -> Self {
+        Self {
+            inner: Arc::new(AppStateInner { config }),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use new_chat::{AppConfig, get_router};
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{Layer as _, fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let config = AppConfig::load()?;
+    let port = config.server.port;
+    let addr = format!("0.0.0.0:{}", port);
+
+    let app = get_router(config);
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Listening on: {}", addr);
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
 #[cfg(test)]
